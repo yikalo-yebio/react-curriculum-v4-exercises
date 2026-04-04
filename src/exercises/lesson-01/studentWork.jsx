f808a16be8fad0d87566f031f1c43809576ef38a
Yikalo Yebio <yikaloye@gmail.com>
2026-04-04 11:07:45 -0500

    Exercise 01

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -2,10 +2,32 @@
 //Exercise: Build an "About Me" Component in this file
 
 export default function StudentWork() {
-  //add variables here
+  const name = 'Yikalo';
+  const age = 35;
+  const listHobbies = [
+    'Watching-football',
+    'Watching-movie',
+    'Playing-football',
+    'Reading',
+    'Coding',
+  ];
   return (
     <div>
-      {/* add JSX here */}
+      {
+        <>
+          <h1>About {name}</h1>
+          <p>
+            {' '}
+            My name is Yikalo coding is been my dream since my childhood by my
+            fathers influence.{' '}
+          </p>
+          <ul>
+            {listHobbies.map((hobbie, index) => (
+              <li key={index}>{hobbie}</li>
+            ))}
+          </ul>
+        </>
+      }
       <p> Student output will go here </p>
     </div>
   );
